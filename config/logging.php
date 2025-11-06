e6ad590a8f43dcae825bdd89f7238f02b0907d41
aethierTest2 <cjo092783@gmail.com>
2025-11-06 10:51:45 +0900

    로깅 채널 추가

--- config/logging.php
+++ config/logging.php
@@ -126,6 +126,45 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
+        'user' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/user/user.log'),
+            'level' => 'info',
+            'permission' => 0664,
+        ],
+
+        'asset' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/asset/asset.log'),
+            'level' => 'info',
+            'days' => 14,
+            'permission' => 0664,
+        ],
+
+        'mining' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/mining/mining.log'),
+            'level' => 'info',
+            'days' => 14,
+            'permission' => 0664,
+        ],
+
+        'bonus' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/bonus/bonus.log'),
+            'level' => 'info',
+            'days' => 14,
+            'permission' => 0664,
+        ],
+
+        'traffic' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/traffic/traffic.log'),
+            'level' => 'info',
+            'days' => 14,
+            'permission' => 0664,
+        ],
     ],
 
 ];
