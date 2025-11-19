dcee65e102346346db1216adbac671e5ff83dfe7
globalunitadesign <globalunitadesign@gmail.com>
2025-11-19 16:40:10 +0900

    up

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href=" {{ asset('images/logos/symbol.png') }}" size="32x32">
+    <link rel="icon" type="image/png" href=" {{ asset('images/logos/bit_symbol.png') }}" size="32x32">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.1/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/dist/tabler-icons.min.css" />
     <link rel="stylesheet" href="{{ asset('css/styles.min.css') }}" />
